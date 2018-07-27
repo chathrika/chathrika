4846e192076d3df2ea2ed6818704f628285302f4
chathrika <41728818+chathrika@users.noreply.github.com>
2018-07-27 14:20:05 +0530

    Create chathri.java

---
+++ chathri.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class chathri
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextint();
+       int k=sc.nextint();
+       int sum=0;
+       int i;
+       int n1;
+       for(i=1;i<=n;i++)
+       {
+       	n1=sc.nextint();
+       }
+       for(i=1;i<=k;i++)
+       {
+       	sum=sum+i;
+       }
+       System.out.println(sum);
+       
+       }
+}
+
+
+
+
+       
+
+       
+       
+    1
+    
+	
