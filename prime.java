a0b3b04ed7b726c7bfba5af051262ab4435dda2c
chathrika <41728818+chathrika@users.noreply.github.com>
2018-07-27 14:44:55 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            // condition for nonprime number
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+    }     
+   }
+
+
+
+
+     
+       
+
+       
+       
+    1
+    
+	
