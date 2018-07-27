5b7f318a3049c068d151527168faec93fff9bf74
chathrika <41728818+chathrika@users.noreply.github.com>
2018-07-27 15:24:37 +0530

    Create oddnumbers.java

---
+++ oddnumbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class  oddnumbers
+{
+
+
+    public static void main(String[] args) {
+
+    
+		System.out.println("The Odd Numbers are:");
+		for (int i = 1; i <= 4; i++) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
+
+
+
+     
+       
+
+       
+       
+    
+    
+	
