c65bd0400e2821e22bd255821b65d43bd16a292c
chathrika <41728818+chathrika@users.noreply.github.com>
2018-07-27 14:32:27 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+   
+
+        int base = 3, exponent = 4;
+
+        long result = 1;
+
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}
+
+
+
+
+     
+       
+
+       
+       
+    1
+    
