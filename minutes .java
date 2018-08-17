d95710f494c6c56f68d74ea4e19fd376572a9de2
chathrika <41728818+chathrika@users.noreply.github.com>
2018-08-17 09:38:53 +0530

    Create minutes .java

---
+++ minutes .java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class minutes
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+04
+    {
+05
+        int runtime = 185; 
+06
+ 
+07
+        int hours = runtime / 60;
+08
+ 
+09
+        int minutes = runtime % 60; // 
+10
+ 
+11
+        System.out.println("The runtime of the movie is " + hours + " hours and " + minutes + " minutes.");
+12
+    
+	}
+}
